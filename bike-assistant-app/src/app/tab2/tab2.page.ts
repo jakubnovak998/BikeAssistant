b946b452e998e09d50958572a29873e6e78c5fe8
patryk-l <patryk.luczak1@unilodz.eu>
2020-05-07 14:16:24 +0200

    initial version of historical traces

--- bike-assistant-app/src/app/tab2/tab2.page.ts
+++ bike-assistant-app/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TraceService} from '../services/datasync.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  history  = [];
+  constructor(private traceService: TraceService) {
+    this.traceService.getTrace().then((response: any) => {
+      this.history = response;
+    });
+  }
+  hmsToSeconds(hms) {
+    const a = hms.split(':');
+    return (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+  }
+  getAvgSpeed(duration, distance) {
+    const avg = parseFloat(distance) / (this.hmsToSeconds(duration) / 3600.0);
+    return avg.toFixed(3);
+  }
+  doRefresh(event) {
+    console.log('Begin async operation');
+    this.traceService.getTrace().then((response: any) => {
+      this.history = response;
+    });
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
 }
