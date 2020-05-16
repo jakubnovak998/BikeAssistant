44901d0a5efcbb0c22dbde583717917612e787f2
jakubnovak998 <30349303+jakubnovak998@users.noreply.github.com>
2020-05-16 14:07:14 +0200

    add finished view of tab3

--- bike-assistant-app/src/app/tab3/tab3.page.ts
+++ bike-assistant-app/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Storage} from '@ionic/storage';
+import {HTTP} from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,7 +9,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private storage: Storage, private http: HTTP) {}
+  ipKey = '10.0.2.2';
+  planValue = 0;
+  planPeriod = 'day';
 
+  createPlan() {
+    this.storage.get('auth-token').then((apiKey) => {
+      console.log('log:', apiKey);
+
+      const postData = {
+        API_KEY: apiKey,
+        value: this.planValue,
+        period: this.planPeriod,
+
+      };
+      console.log(postData);
+      /*return new Promise((resolve, reject) => {
+        this.http.post('https://' + this.ipKey + ':5000/api/saveTrace', postData, {'Content-Type': 'application/json'})
+            .then(data => {
+                  resolve(data);
+                }
+            )
+            .catch(error => {
+              reject(error);
+            });
+      }); */
+    });
+  }
 
 }
