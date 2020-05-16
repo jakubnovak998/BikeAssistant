3772a743facf4b0c409b5fd2b039556e7f7c2df0
patryk-l <patryk.luczak1@unilodz.eu>
2020-05-16 16:29:33 +0200

    added historical traces

--- bike-assistant-app/src/app/tab2/tab2.page.ts
+++ bike-assistant-app/src/app/tab2/tab2.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import {TraceService} from '../services/datasync.service';
-
+import {
+  GoogleMaps,
+  GoogleMap,
+  Marker,
+  GoogleMapOptions
+} from '@ionic-native/google-maps/ngx';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -8,6 +13,11 @@ import {TraceService} from '../services/datasync.service';
 })
 export class Tab2Page {
   history  = [];
+  showMap = false;
+  temp;
+  trace = [];
+  private map: GoogleMap;
+  marker: Marker;
   constructor(private traceService: TraceService) {
     this.traceService.getTrace().then((response: any) => {
       this.history = response;
@@ -20,6 +30,47 @@ export class Tab2Page {
   getAvgSpeed(duration, distance) {
     const avg = parseFloat(distance) / (this.hmsToSeconds(duration) / 3600.0);
     return avg.toFixed(3);
+  }
+  hideShow() {
+    this.showMap = ! this.showMap;
+  }
+  showHistoricalTrace(i)  {
+    this.temp = i;
+    this.trace = i.TRACE;
+    this.hideShow();
+    this.loadMap(this.trace[this.trace.length - 1]);
+    this.map.addPolyline({
+      points: this.trace,
+      geodesic: true,
+      color: '#ff00ff',
+      strokeWeight: 3
+    });
+
+  }
+  loadMap(pos) {
+
+    const mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: pos.lat,
+          lng: pos.lng
+        },
+        zoom: 20,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    this.marker = this.map.addMarkerSync({
+      title: 'END',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: pos.lat,
+        lng: pos.lng
+      }
+    });
   }
   doRefresh(event) {
     console.log('Begin async operation');
